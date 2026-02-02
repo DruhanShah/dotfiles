3fac1e54449f2766c8c1160f46e631d65007becf
DruhanShah <druhanrshah@gmail.com>
2026-02-02 16:36:28 +0530

    Start move to greyscale

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -7,7 +7,7 @@
   config = lib.mkIf config.modules.stylix.enable {
     stylix.enable = true;
     stylix.polarity = "light";
-    stylix.base16Scheme = ../../dots/stylix/everforest-light-medium.yaml;
+    stylix.base16Scheme = ../../dots/stylix/e-ink.yaml;
     stylix.cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
